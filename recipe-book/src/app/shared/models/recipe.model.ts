20e5c2c64e473de1d4963e4bff7343c50e5c8410
aaron <afarrell002@gmail.com>
2022-06-11 17:26:55 -0600

    added ingredients to recipe model to initiate add to shopping list functionality

--- recipe-book/src/app/shared/models/recipe.model.ts
+++ recipe-book/src/app/shared/models/recipe.model.ts
@@ -1,9 +1,17 @@
+import { Ingredient } from './ingredient.model';
+
 export class Recipe {
   public name: String;
   public description: String;
   public imgSrc: string;
+  public ingredients: Ingredient[];
 
-  constructor(name: string, description: string, imgSrc: string) {
+  constructor(
+    name: string,
+    description: string,
+    imgSrc: string,
+    ingredients: Ingredient[]
+  ) {
     this.name = name;
     this.description = description;
     this.imgSrc = imgSrc;
