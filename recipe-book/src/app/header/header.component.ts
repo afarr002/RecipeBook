ab4676192bc3df9c3acb78c9fd9d611a5f5b14e2
aaron <afarrell002@gmail.com>
2022-06-14 20:07:16 -0600

    added functionality to header buttons to load corresponding component through the router

--- recipe-book/src/app/header/header.component.ts
+++ recipe-book/src/app/header/header.component.ts
@@ -1,16 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-  collapsed = true;
-
-  @Output() destinationRoute = new EventEmitter<string>();
-
-  onSelect(destination: string) {
-    this.destinationRoute.emit(destination);
-  }
-}
+export class HeaderComponent {}
