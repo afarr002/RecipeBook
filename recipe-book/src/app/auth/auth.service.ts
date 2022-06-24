3838c9e54f3b3080cca722551c456c3cf496c6fa
aaron <afarrell002@gmail.com>
2022-06-23 21:18:11 -0600

    updated and refactored logic to handle error messages for both signup and login http requests in the same place

--- recipe-book/src/app/auth/auth.service.ts
+++ recipe-book/src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { catchError } from 'rxjs/operators';
@@ -33,35 +33,44 @@ export class AuthService {
           returnSecureToken: true,
         }
       )
-      .pipe(
-        catchError((errorRes) => {
-          let errorMessage = 'An unknown error has occured, try again later!';
-
-          if (!errorRes.error || !errorRes.error.error) {
-            return throwError(() => new Error(errorMessage));
-          }
-          switch (errorRes.error.error.message) {
-            case 'EMAIL_EXISTS':
-              errorMessage = 'A user with this email already exists!';
-            case 'OPERATION_NOT_ALLOWED':
-              errorMessage = 'Password sign-in is disabled!';
-            case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-              errorMessage =
-                'Too many incorrect attempts have been made, try again later!';
-          }
-          return throwError(() => new Error(errorMessage));
-        })
-      );
+      .pipe(catchError(this.errorHandling));
   }
 
   login(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDb6jXvssSwUVNbAQcjwQRrJlbkxi4O2H4',
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true,
-      }
-    );
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDb6jXvssSwUVNbAQcjwQRrJlbkxi4O2H4',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(catchError(this.errorHandling));
+  }
+
+  private errorHandling(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error has occured, try again later!';
+
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(() => new Error(errorMessage));
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'A user with this email already exists!';
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMessage =
+          'Too many incorrect attempts have been made, try again later!';
+      case 'EMAIL_NOT_FOUND':
+        errorMessage =
+          'There is no user with this email/password combo, please check password or signup!';
+      case 'INVALID_PASSWORD':
+        errorMessage =
+          'There is no user with this email/password combo, please check password or signup!';
+      case 'USER_DISABLED':
+        errorMessage =
+          'This account has been locked, please contect an administrator!';
+    }
+    return throwError(() => new Error(errorMessage));
   }
 }
