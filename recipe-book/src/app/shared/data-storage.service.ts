b847510c4a350200dc719107d15e6f513654f036
aaron <afarrell002@gmail.com>
2022-06-23 18:11:29 -0600

    added http requests to store then fetch recipes on button click from navbar

--- recipe-book/src/app/shared/data-storage.service.ts
+++ recipe-book/src/app/shared/data-storage.service.ts
@@ -1,9 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { Recipe } from '../shared/models/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
+
 @Injectable({
   providedIn: 'root',
 })
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        'https://angular-recipe-book-15749-default-rtdb.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe((res) => console.log(res));
+  }
+
+  fetchRecipes() {
+    this.http
+      .get<Recipe[]>(
+        'https://angular-recipe-book-15749-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .subscribe((recipes) => {
+        this.recipeService.setRecipes(recipes);
+      });
+  }
 }
