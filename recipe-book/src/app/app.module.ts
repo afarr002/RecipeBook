c0ba8284b741ba7f3cf4c9cecc1119e92fbc86a8
aaron <afarrell002@gmail.com>
2022-06-25 17:49:21 -0600

    added notes about core module within the app module

--- recipe-book/src/app/app.module.ts
+++ recipe-book/src/app/app.module.ts
@@ -57,5 +57,12 @@ import { PlaceholderDirective } from './shared/directives/placeholder.directive'
     ]
 
   */
+
+  /*
+
+    you can create a core module to house mulitple services IF you are importing mulitple services into the providers within the app module.
+    all the services in my app are injected to root (provided: 'inRoot') in the service files, so it is not neccesary here
+
+  */
 })
 export class AppModule {}
