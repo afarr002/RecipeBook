21292792f2420a2229ed2334102514624fe92ec9
aaron <afarrell002@gmail.com>
2022-06-24 18:57:22 -0600

    added autoLogin method to the auth service to persist the logged in user as long as users token is still valid

--- recipe-book/src/app/auth/auth.service.ts
+++ recipe-book/src/app/auth/auth.service.ts
@@ -74,6 +74,29 @@ export class AuthService {
       );
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    } else {
+      const loadedUser = new User(
+        userData.email,
+        userData.id,
+        userData._token,
+        new Date(userData._tokenExpirationDate)
+      );
+
+      if (loadedUser.token) {
+        this.user.next(loadedUser);
+      }
+    }
+  }
+
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
@@ -88,6 +111,7 @@ export class AuthService {
     const tokenExp = new Date(new Date().getTime() + expiresIn * 1000); // creates a date for the token expiration since it is returned to us in seconds from firebase
     const user = new User(email, userId, token, tokenExp);
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private errorHandling(errorRes: HttpErrorResponse) {
