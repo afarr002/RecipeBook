e41ff18961e3d89341ab9145f447e951fb6337a4
aaron <afarrell002@gmail.com>
2022-06-25 18:33:01 -0600

    added lazy loading to shrink our files at runtime

--- recipe-book/src/app/app.module.ts
+++ recipe-book/src/app/app.module.ts
@@ -3,10 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AuthModule } from './auth/auth.module';
-import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -28,15 +25,7 @@ import { PlaceholderDirective } from './shared/directives/placeholder.directive'
     PlaceholderDirective,
     */
   ],
-  imports: [
-    AppRoutingModule,
-    AuthModule,
-    BrowserModule,
-    HttpClientModule,
-    RecipesModule,
-    SharedModule,
-    ShoppingListModule,
-  ],
+  imports: [AppRoutingModule, BrowserModule, HttpClientModule, SharedModule],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
